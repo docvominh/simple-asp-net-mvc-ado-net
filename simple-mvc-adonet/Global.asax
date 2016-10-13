e4dccb453f4b4d0dad6200bb3fddef9e476617c8
Pham Duc Minh <phamducminh1990@gmail.com>
2016-10-13 21:17:36 +0700

    Add project files.

---
+++ simple-mvc-adonet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="simple_mvc_adonet.MvcApplication" Language="C#" %>
---
+++ simple-mvc-adonet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(simple_mvc_adonet.Startup))]
+namespace simple_mvc_adonet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
